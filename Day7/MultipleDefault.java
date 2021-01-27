bd418586d6a6700235f1829e4e1362ee8176fd9c
BhavaniIT <bhavanitechit@gmail.com>
2021-01-27 16:58:55 +0530

    Multiple Defaults in interface

---
+++ Day7/MultipleDefault.java
@@ -0,0 +1,35 @@
+package Day7;
+
+interface Left
+{
+    default void m1()
+    {
+        System.out.println("Left interface");
+    }
+}
+
+interface Right
+{
+    default void m1()
+    {
+        System.out.println("Right interface");
+    }
+}
+
+public class MultipleDefault implements Left,Right
+{
+    public void m1()
+    {   //Left.super.m1(); => output:Left
+        //Right.super.m1(); => output: Right
+        System.out.println("Own Default method");
+    }
+    public static void main(String[] args) 
+    {
+        Left l = new MultipleDefault();
+        l.m1(); //overridden
+        Right r = new MultipleDefault();
+        r.m1(); //overridden
+        MultipleDefault m = new MultipleDefault();
+        m.m1(); //overridden
+    }
+}
