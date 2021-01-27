5b2f7cc8956d7b68bbb97ee46192cae9482f3ff0
BhavaniIT <bhavanitechit@gmail.com>
2021-01-27 22:54:26 +0530

    checking the length of string using predicate

--- Day7/CheckStringLength.java
+++ Day7/CheckStringLength.java
@@ -7,6 +7,20 @@ public class CheckStringLength
         Predicate<String> p = s -> s.length() > 5;
         System.out.println(p.test("Bhavani"));
         System.out.println(p.test("Java"));
+
+        //Another example
+
+        String[] b= {"Bhavani", "java", "version8", "learning","okay"};
+        for(String b1:b)
+        {
+            Predicate<String> p1 = str -> str.length() > 5;
+            if(p1.test(b1))
+            {
+                System.out.println("Length greater than 5:"+b1);
+            }
+            
+        }
+
     }
     
 }
