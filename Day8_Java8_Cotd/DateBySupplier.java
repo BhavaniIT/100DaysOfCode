1e0f69c53f8e2d0c45faa2fe3f39a806ded229ac
BhavaniIT <bhavanitechit@gmail.com>
2021-01-28 21:25:31 +0530

    To get date using supplier interface

---
+++ Day8_Java8_Cotd/DateBySupplier.java
@@ -0,0 +1,13 @@
+package Day8_Java8_Cotd;
+import java.util.Date;
+import java.util.function.Supplier;
+
+
+public class DateBySupplier 
+{
+    public static void main(String[] args) 
+    {
+        Supplier<Date> s = ()-> new Date();
+        System.out.println(s.get());
+    }
+}
