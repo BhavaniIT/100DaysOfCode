74ba96703896dea2e8a468f00f63f8c24c8a260d
BhavaniIT <bhavanitechit@gmail.com>
2021-01-28 21:54:49 +0530

    Bipredicate demo sum of two numbers even ot not

---
+++ Day8_Java8_Cotd/BiPredicateDemo.java
@@ -0,0 +1,12 @@
+package Day8_Java8_Cotd;
+import java.util.function.BiPredicate;
+
+public class BiPredicateDemo 
+{
+    public static void main(String[] args) 
+    {
+        //It accepts 2 paramteres. normal predicate accepts only one parameter
+       BiPredicate<Integer,Integer> p = (a,b)->(a+b)%2==0;
+       System.out.println(p.test(10,20)); 
+    }
+}
