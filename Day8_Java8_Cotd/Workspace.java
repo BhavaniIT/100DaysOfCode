1bfe18724935e1c3d69a77ce836b5d65b02ce281
BhavaniIT <bhavanitechit@gmail.com>
2021-02-10 14:26:42 +0530

    workspace

---
+++ Day8_Java8_Cotd/Workspace.java
@@ -0,0 +1,20 @@
+package Day8_Java8_Cotd;
+import java.util.Scanner;
+
+public class Workspace
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer(scan.nextLine());
+        int op2 = Integer.parseInt(sb.reverse().toString());
+        int op1 = Integer.parseInt(sb.reverse().toString());
+        System.out.println(op1-op2);
+
+
+    
+
+        
+
+    }
+}
