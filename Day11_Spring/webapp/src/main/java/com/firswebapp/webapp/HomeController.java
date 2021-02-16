b57554f9da6ba60bb785e0be4b4e3e67f0e11b23
BhavaniIT <bhavanitechit@gmail.com>
2021-02-16 17:19:46 +0530

    HttpServletRequest in controller

--- Day11_Spring/webapp/src/main/java/com/firswebapp/webapp/HomeController.java
+++ Day11_Spring/webapp/src/main/java/com/firswebapp/webapp/HomeController.java
@@ -1,5 +1,9 @@
 package com.firswebapp.webapp;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@RequestMapping("home")
-	public String show()
+	public String show(HttpServletRequest req)
 	{
+		HttpSession session = req.getSession(); 
+		String name = req.getParameter("name");
 		System.out.println("hi");
-		return "home.jsp";
+		session.setAttribute("name", name);
+		return "home";
 	}
 }
